d91712d36a57925382b685541f02747283895038
Costas Yiotis <coyiotis@gmail.com>
2020-11-02 23:25:47 +0200

    fix crlf

--- kafka/entrypoint.sh
+++ kafka/entrypoint.sh
@@ -3,6 +3,7 @@
 IP_ADDR=$(hostname -I | cut -d ' ' -f1) 
 echo $IP_ADDR
 
+cp /opt/kafka/config/server.properties_template /opt/kafka/config/server.properties
 /bin/sed -i 's/IP_ADDR/'''$IP_ADDR'''/g' /opt/kafka/config/server.properties
 
 kafka-server-start.sh /opt/kafka/config/server.properties
