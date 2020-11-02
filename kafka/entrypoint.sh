d164addd85593760965049f9ad31cec6461b119e
Costas Yiotis <coyiotis@gmail.com>
2020-11-02 21:02:00 +0200

    fix kafka docker-compose

--- kafka/entrypoint.sh
+++ kafka/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-IP_ADDR=$(hostname -I | cut -d ' ' -f1) &
-/bin/sed -i 's/IP_ADDR/'"$IP_ADDR"'/g' /opt/kafka/config/server.properties &
-kafka-server-start.sh", "/opt/kafka/config/server.properties
+IP_ADDR=$(hostname -I | cut -d ' ' -f1) 
+echo $IP_ADDR
+
+/bin/sed -i 's/IP_ADDR/'''$IP_ADDR'''/g' /opt/kafka/config/server.properties
+
+kafka-server-start.sh /opt/kafka/config/server.properties
